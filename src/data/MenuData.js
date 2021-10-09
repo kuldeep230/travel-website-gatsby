9a159fdda35b1d327fb69205a454b5a9536ed41a
kuldeep230 <ramdeep214@gmail.com>
2021-10-10 02:21:50 +0530

    date array to display all the navLinks

---
+++ src/data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+  { title: "About", link: "./link" },
+  { title: "Trips", link: "./trips" },
+  { title: "Careers", link: "./carrers" },
+  { title: "Contact", link: "./contact" },
+]
